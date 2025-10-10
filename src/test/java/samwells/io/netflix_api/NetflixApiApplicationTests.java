8c90fc7b16595a1eec3981a4f7157a11acd2a836
Sam Wells <samwells95@gmail.com>
2025-10-10 11:34:24 -0700

    Initial project

---
+++ src/test/java/samwells/io/netflix_api/NetflixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package samwells.io.netflix_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
