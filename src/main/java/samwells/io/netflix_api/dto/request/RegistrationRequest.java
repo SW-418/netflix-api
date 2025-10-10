df44c66134a6cba62311eaa13af54f87085e2298
Sam Wells <samwells95@gmail.com>
2025-10-10 12:41:58 -0700

    Registration req+response

---
+++ src/main/java/samwells/io/netflix_api/dto/request/RegistrationRequest.java
@@ -0,0 +1,10 @@
+package samwells.io.netflix_api.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record RegistrationRequest(
+        @NotNull @Size(min = 1, max = 100) @Email String username,
+        @NotNull @Size(min = 1, max = 200) String password
+) { }
