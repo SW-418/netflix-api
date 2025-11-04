805d8b41b47dad49fbdf2bd374118e2a0f0dc6fe
Sam Wells <samwells95@gmail.com>
2025-11-04 14:34:43 -0800

    Add constructor w/ MediaType for notfound

--- src/main/java/samwells/io/netflix_api/exception/ResourceNotFoundException.java
+++ src/main/java/samwells/io/netflix_api/exception/ResourceNotFoundException.java
@@ -1,7 +1,13 @@
 package samwells.io.netflix_api.exception;
 
+import samwells.io.netflix_api.model.MediaType;
+
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(Long id) {
         super(String.format("Requested resource with id %s could not be found.", id));
+    }
+
+    public ResourceNotFoundException(Long id, MediaType mediaType) {
+        super(String.format("Requested resource with id %s could not be found for media type %s.", id, mediaType));
     }
 }
