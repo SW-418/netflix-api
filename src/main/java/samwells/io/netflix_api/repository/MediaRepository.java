522c923013896528829530c0e77366ad4a03e3a8
Sam Wells <samwells95@gmail.com>
2025-11-04 14:35:47 -0800

    Add existence check for media on id and typ

--- src/main/java/samwells/io/netflix_api/repository/MediaRepository.java
+++ src/main/java/samwells/io/netflix_api/repository/MediaRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import samwells.io.netflix_api.entity.Media;
 import samwells.io.netflix_api.model.Genre;
+import samwells.io.netflix_api.model.MediaType;
 
 @Repository
 public interface MediaRepository extends JpaRepository<Media, Long> {
@@ -20,5 +21,21 @@ public interface MediaRepository extends JpaRepository<Media, Long> {
     Page<Media> getMovies(
             @Param("genre") Genre genre,
             Pageable pageable
+    );
+
+    @Query("""
+            SELECT EXISTS (
+                SELECT
+                    1
+                FROM
+                    Media m
+                WHERE
+                    m.id = :id AND
+                    m.mediaType.name = :media_type
+            )
+            """)
+    boolean existsByIdAndMediaType(
+            @Param("id") Long id,
+            @Param("media_type") MediaType mediaType
     );
 }
